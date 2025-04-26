10582d68b4ff981569dde2da4574df88cae2f2a6
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-04-26 12:46:49 +0545

    refactor(auth): remove debug console logs from AuthorizationGuard

--- src/common/guard/authorization.guard.ts
+++ src/common/guard/authorization.guard.ts
@@ -10,7 +10,6 @@ export class AuthorizationGuard implements CanActivate {
     constructor(private reflector: Reflector) { }
 
     canActivate(context: ExecutionContext) {
-        console.log('AuthorizationGuard runs 12323');
         const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
             context.getHandler(),
             context.getClass(),
@@ -19,7 +18,6 @@ export class AuthorizationGuard implements CanActivate {
         const { user } = context.switchToHttp().getRequest<Request>();
 
         if (!user) throw new ForbiddenException(Message.notAuthorized);
-        console.log(user.role, 'user role');
 
         if (!requiredRoles.some((role) => user.role?.includes(role))) throw new ForbiddenException(Message.notPermitted);
         return true;
