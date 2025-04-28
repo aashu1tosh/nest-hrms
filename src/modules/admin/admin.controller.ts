2b14807d7ca99fcb482ca6e1e1259ad8aa634a64
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-04-28 19:30:45 +0545

    feat(admin): inject AdminService into AdminController constructor

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,7 +1,10 @@
 import { Controller } from '@nestjs/common';
+import { AdminService } from './service/admin.service';
 
 @Controller('admin')
 export class AdminController {
-  constructor() { }
+  constructor(
+    private readonly adminService: AdminService,
+  ) { }
 
 }
