e85f33f0ced2e0c18b188ee1657d5a7b4cb244b3
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-05-03 21:47:18 +0545

    feat(admin): enhance CreateAdminDTO with Swagger API properties for better documentation feat(auth): add Swagger API properties to CreateAuthDTO and LoginDTO for improved API documentation

--- src/modules/admin/dto/admin.dto.ts
+++ src/modules/admin/dto/admin.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { IsNotBlank } from 'src/common/dto/is-not-blank.dto';
 
 export class CreateAdminDTO {
+  @ApiProperty({ example: 'First Name', description: 'First name of the user' })
   @IsNotEmpty()
   @IsNotBlank()
   firstName: string;
 
+  @ApiProperty({ example: 'Middle Name', description: 'Middle name of the user' })
   @IsOptional()
   @IsString()
   middleName: string;
 
+  @ApiProperty({ example: 'Last Name', description: 'Last name of the user' })
   @IsNotEmpty()
   @IsNotBlank()
   lastName: string;
